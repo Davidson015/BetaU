2549057313602b1c87c0bc315be91d027e190932
David Ogunsola <tobilobadiekola@gmail.com>
2024-04-09 12:21:56 +0100

    Refactor: Just added some new files and that.

--- app/src/main/java/com/example/betau/ui/theme/Color.kt
+++ app/src/main/java/com/example/betau/ui/theme/Color.kt
@@ -9,3 +9,15 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+// CUSTOMS
+// Light
+val Green = Color(0XFF4CAF50)
+val Pink = Color(0xFFFF4081)
+val Accent = Color(0xFF2196F3)
+
+// Dark
+val DarkGreen = Color(0XFF388E3C)
+val DarkPink = Color(0xFFFF80AB)
+val AccentDark = Color(0xFF03A9F4)
+val DarkBack = Color(0xFF121212)
